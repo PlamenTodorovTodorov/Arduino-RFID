17751e4d227b8e389fba7dce405f12dbda72f762
arsenovb <157198325+arsenovb@users.noreply.github.com>
2025-06-30 20:57:25 +0300

    Update Arduino-RFID.ino
    access granted/denied

--- Arduino-RFID/Arduino-RFID.ino
+++ Arduino-RFID/Arduino-RFID.ino
@@ -1,22 +1,13 @@
-/*
- * -------------------------------------
- *             MFRC522      Arduino     
- *             Reader       Uno    
- * Signal      Pin          Pin         
- * --------------------------------------
- * RST/Reset   RST          9            
- * SPI SS      SDA(SS)      10           
- * SPI MOSI    MOSI         11 / ICSP-4  
- * SPI MISO    MISO         12 / ICSP-1  
- * SPI SCK     SCK          13 / ICSP-3  
- */
-
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
 
-#define RST_PIN         9          
-#define SS_PIN          10         
+#define RST_PIN 9
+#define SS_PIN 10
+
+byte readCard[4];
+String MasterTag = "139B4CCD";  // REPLACE this Tag ID with your Tag ID!
+String tagID = "";
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
@@ -24,39 +15,66 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 void setup() {
-	Serial.begin(9600);		
-	while (!Serial);		
-	SPI.begin();			
-	mfrc522.PCD_Init();		
-	delay(4);				
-	mfrc522.PCD_DumpVersionToSerial();	
-	Serial.println(F("Scan TAG to see info..."));
-	lcd.begin(16, 2);  
+  Serial.begin(9600);		
+	while (!Serial);
+
+  SPI.begin();         // SPI 
+  mfrc522.PCD_Init();  // MFRC522
+  delay(4);
+
+  lcd.begin(16, 2);    // LCD 
+  lcd.clear();
+  lcd.print(" Access Control ");
+  lcd.setCursor(0, 1);
+  lcd.print("Scan Your Card>>");
 }
 
 void loop() {
-  if (!mfrc522.PICC_IsNewCardPresent()) return;
-  if (!mfrc522.PICC_ReadCardSerial()) return;
 
- mfrc522.PICC_DumpToSerial(&(mfrc522.uid));  // show in Serial Monitor
+  //Wait until new tag is available
+  while (getID()) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
 
-  lcd.clear();                
-  lcd.setCursor(0, 0);
-  lcd.print("UID:");
+    if (tagID == MasterTag) {
 
-for (byte i = 0; i < mfrc522.uid.size; i++) {
-  if (mfrc522.uid.uidByte[i] < 0x10)
-    lcd.print("0"); 
+      lcd.print(" Access Granted!");
+      // You can write any code here like opening doors, switching on a relay, lighting up an LED, or anything else you can think of.
+    } else {
+      lcd.print(" Access Denied!");
+    }
 
-  lcd.print(mfrc522.uid.uidByte[i], HEX);
+    lcd.setCursor(0, 1);
+    lcd.print(" ID : ");
+    lcd.print(tagID);
 
-  if (i != mfrc522.uid.size - 1) {
-    lcd.print(" "); // Add space between bytes
+    delay(2000);
+
+    lcd.clear();
+    lcd.print(" Access Control ");
+    lcd.setCursor(0, 1);
+    lcd.print("Scan Your Card>>");
   }
 }
 
-delay(2000);  // Pause to let user read it
-lcd.clear();
-lcd.print("Scan RFID tag");
+//Read new tag if available
+boolean getID() {
+  // Getting ready for Reading PICCs
+  if (!mfrc522.PICC_IsNewCardPresent()) {  //If a new PICC placed to RFID reader continue
+    return false;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {  //Since a PICC placed get Serial and continue
+    return false;
+  }
+  tagID = "";
+  for (uint8_t i = 0; i < 4; i++) {  // The MIFARE PICCs that we use have 4 byte UID
+    //readCard[i] = mfrc522.uid.uidByte[i];
+    tagID.concat(String(mfrc522.uid.uidByte[i], HEX));  // Adds the 4 bytes in a single String variable
+  }
 
+  tagID.toUpperCase();
+
+  mfrc522.PICC_HaltA();  // Stop reading
+
+  return true;
 }
